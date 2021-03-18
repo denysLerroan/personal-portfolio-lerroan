a98d4f7761ba90303c1feb8d3c79e8bb09622262
denysLerroan <lerroandeco@gmail.com>
2021-03-18 19:05:29 -0300

    refactoring to SPA structure

---
+++ js/modules/root.js
@@ -0,0 +1,35 @@
+import home from '../views/home.js';
+import about from '../views/about-me.js';
+import work from '../views/work.js';
+import services from '../views/services.js';
+
+export default function initRoot() {
+  const main = document.querySelector('#root');
+
+  const init = () => {
+    window.addEventListener('hashchange', () => {
+      main.innerHTML = ' ';
+      switch (window.location.hash) {
+        case ' ':
+          main.appendChild(home());
+          break;
+        case '#services':
+          main.appendChild(services());
+          break;
+        case '#work':
+          main.appendChild(work());
+          break;
+        case '#about':
+          main.appendChild(about());
+          break;
+        default:
+          main.appendChild(home());
+      }
+    });
+  };
+
+  window.addEventListener('load', () => {
+    main.appendChild(home());
+    init();
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import initRoot from './modules/root.js';
 import initNavMenu from './modules/navbar.js';
 
+initRoot();
 initNavMenu();
