a6da9c01c6e5fea3084e73503e8daa787dca5c13
denysLerroan <lerroandeco@gmail.com>
2021-03-19 09:16:17 -0300

    pre-loader page stylesheet updated

--- js/modules/navbar.js
+++ js/modules/navbar.js
@@ -1,14 +1,14 @@
 export default function initNavMenu() {
   const hamburger_menu = document.querySelector('.hamburger-menu');
-  const container = document.querySelector('.container');
+  const bgNavbar = document.querySelector('.bg-navbar');
   const links = document.querySelectorAll('.links ul li a');
 
   links.forEach((link) => {
     link.addEventListener('click', () => {
-      container.classList.toggle('active');
+      bgNavbar.classList.toggle('active');
     });
   });
   hamburger_menu.addEventListener('click', () => {
-    container.classList.toggle('active');
+    bgNavbar.classList.toggle('active');
   });
 }
